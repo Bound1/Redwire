c7acd09b76b5923b63196d2c5ed81a33d2ed7c67
onangeko <olivier.nangeko@ynov.com>
2023-04-28 16:58:05 +0200

    feat: Add save_image_gallery function for saving gallery images
    This commit introduces a new function called save_image_gallery. The function takes the gallery value as input and saves the images with unique UUID-based filenames. It returns a gr.File.update object that includes the paths to the saved files.
    
    This feature enhances the functionality of the application by providing a convenient way to download all the images from a gallery.

--- app/utils.py
+++ app/utils.py
@@ -1,3 +1,5 @@
+import base64
+import io
 from PIL import Image
 from app.models.stable_text_to_image import TextToImageGenerator
 
@@ -10,7 +12,7 @@ def generate_text_to_image(prompt, height, width, num_inference, guidance_scale,
     try:
         output_images = []
         images = text_to_image_generator.generate_image(prompt, height, width, num_inference, guidance_scale,
-                                                            negative_prompt, num_images_per_prompt)
+                                                        negative_prompt, num_images_per_prompt)
         output_images.extend(images)
         return output_images
     except Exception as e:
@@ -32,3 +34,35 @@ def generate_image_to_image(prompt, input_image, strength, num_inference_steps, 
         return output_images
     except Exception as e:
         raise ValueError(str(e))
+
+
+def plaintext_to_html(text: str) -> str:
+    return f"<p style='font-family: monospace;'>{text}</p>"
+
+def image_from_url_text(filedata):
+    if filedata is None:
+        return None
+
+    if type(filedata) == list and len(filedata) > 0 and type(filedata[0]) == dict and filedata[0].get("is_file", False):
+        filedata = filedata[0]
+
+    if type(filedata) == dict and filedata.get("is_file", False):
+        filename = filedata["name"]
+        return Image.open(filename)
+
+    if type(filedata) == list:
+        if len(filedata) == 0:
+            return None
+
+        filedata = filedata[0]
+
+    if filedata.startswith("data:image/png;base64,"):
+        filedata = filedata[len("data:image/png;base64,"):]
+
+    filedata = base64.decodebytes(filedata.encode('utf-8'))
+    image = Image.open(io.BytesIO(filedata))
+    return image
+
+
+
+
